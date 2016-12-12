ff3322e86d2ad7af6df4601fbd6407a0416d204f
Vincent <vcholmen@gmail.com>
2016-12-12 15:34:25 -0800

    Update elevated-gallery.js

--- elevated-gallery.js
+++ elevated-gallery.js
@@ -34,7 +34,7 @@ var renderGallery = function(){
         renderGallery.renderItems(galleryId,charterVideoUrls,charterPosterUrl);
     };
     renderGallery.writeDependencies = function(){
-        //video-js.css
+       //video-js.css
 var charter_videojs_style = document.createElement('script');
 charter_videojs_style.type = 'text/css';
 charter_videojs_style.rel = 'stylesheet';
@@ -59,10 +59,14 @@ charter_tools_videoga.type = 'text/javascript';
 charter_tools_videoga.src = 'http://charterauto.tv/js/tools/premiumContentLibrary/video-ga.js';
 
 //Google Analytics
+var charter_tracking_gAinit = document.createElement('script');
+charter_tracking_gAinit.type = 'text/javascript';
+charter_tracking_gAinit.src = 'http://www.google-analytics.com/analytics.js';
 var charter_tracking_gAload = document.createElement('script');
 var charter_tracking_gAload_node = document.createTextNode('var _0xe9d5=["\x6F\x6E\x63\x6F\x6E\x74\x65\x78\x74\x6D\x65\x6E\x75","\x73\x63\x70\x6C\x75\x73\x2D\x70\x6C\x61\x79\x65\x72","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64"];document[_0xe9d5[2]](_0xe9d5[1])[_0xe9d5[0]]=function (){return false;} (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");');
 
 //Write links
+document.getElementsByTagName('head')[0].appendChild(charter_tracking_gAinit);
 document.getElementsByTagName('head')[0].appendChild(charter_tracking_gAload).appendChild(charter_tracking_gAload_node);
 document.getElementsByTagName('head')[0].appendChild(charter_videojs_style);
 document.getElementsByTagName('head')[0].appendChild(charter_videojs_script);
